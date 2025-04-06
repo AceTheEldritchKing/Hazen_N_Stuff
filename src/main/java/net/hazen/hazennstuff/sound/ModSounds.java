3dca26afc9744ff7f6b2ab02a534bfaa502cb553
Ace The King <solcat0414@gmail.com>
2025-04-06 01:57:00 -0400

    Changelog:
    I fixed your sounds and rearranged your mod
    
    - Ace

--- src/main/java/net/hazen/hazennstuff/sound/ModSounds.java
+++ src/main/java/net/hazen/hazennstuff/sound/ModSounds.java
@@ -2,11 +2,13 @@ package net.hazen.hazennstuff.sound;
 
 import net.hazen.hazennstuff.HazenNStuff;
 import net.minecraft.client.resources.sounds.Sound;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.common.util.DeferredSoundType;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
@@ -15,16 +17,18 @@ public class ModSounds {
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS =
             DeferredRegister.create(BuiltInRegistries.SOUND_EVENT, HazenNStuff.MOD_ID);
 
-    public static final Supplier<SoundEvent> SOUL_FLAME_EQUIP_1 = registerSoundEvent("soul_flame_equip_1");
-    public static final Supplier<SoundEvent> SOUL_FLAME_EQUIP_2 = registerSoundEvent("soul_flame_equip_2");
-    public static final Supplier<SoundEvent> SOUL_FLAME_EQUIP_3 = registerSoundEvent("soul_flame_equip_3");
-    public static final Supplier<SoundEvent> SOUL_FLAME_EQUIP_4 = registerSoundEvent("soul_flame_equip_4");
-    public static final Supplier<SoundEvent> SOUL_FLAME_EQUIP_5 = registerSoundEvent("soul_flame_equip_5");
+    public static final Holder<SoundEvent> SOUL_FLAME_EQUIP_1 = registerSoundEvent("soul_flame_equip_1");
+    public static final Holder<SoundEvent> SOUL_FLAME_EQUIP_2 = registerSoundEvent("soul_flame_equip_2");
+    public static final Holder<SoundEvent> SOUL_FLAME_EQUIP_3 = registerSoundEvent("soul_flame_equip_3");
+    public static final Holder<SoundEvent> SOUL_FLAME_EQUIP_4 = registerSoundEvent("soul_flame_equip_4");
+    public static final Holder<SoundEvent> SOUL_FLAME_EQUIP_5 = registerSoundEvent("soul_flame_equip_5");
 
+    public static final Holder<SoundEvent> SOUL_FLAME_EQUIP = registerSoundEvent("soul_flame_equip");
 
-    private static Supplier<SoundEvent> registerSoundEvent(String name) {
-        ResourceLocation id = ResourceLocation.fromNamespaceAndPath(HazenNStuff.MOD_ID, name);
-        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent(id));
+    private static DeferredHolder<SoundEvent, SoundEvent> registerSoundEvent(String name)
+    {
+        return SOUND_EVENTS.register(name, () -> SoundEvent.createVariableRangeEvent
+                (ResourceLocation.fromNamespaceAndPath(HazenNStuff.MOD_ID, name)));
     }
 
     public static void register(IEventBus eventBus) {
